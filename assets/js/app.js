ccfa9a1a4fa6d95da077c7197105e71ec8e98c1f
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2020-10-15 17:21:30 +0530

    edit-box

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 var qr;
 var canva;
-const downloadBtn = document.getElementById('download');
-const backColor = document.getElementById('back-color');
+const downloadBtn = document.getElementById("download");
+const backColor = document.getElementById("back-color");
+const foreColor = document.getElementById("fore-color");
+const editorCheck = document.getElementById('input-check');
 (function () {
-     canva = document.getElementById('canvas');
-     canva.style.display = 'none'
-    qr = new QRious({
+  canva = document.getElementById("canvas");
+  canva.style.display = "none";
+  qr = new QRious({
     element: canva,
     size: 250,
     value: "",
@@ -15,16 +17,20 @@ const backColor = document.getElementById('back-color');
 function generateQRCode() {
   var qrtext = document.getElementById("input-text").value;
   qr.set({
-    background: backColor,
-    foreground: "black",
+    background: backColor.value,
+    foreground: foreColor.value,
     size: 200,
     value: qrtext,
   });
-  canva.style.display = 'block';
-  downloadBtn.style.display = 'block';
+  canva.style.display = "block";
+  setInterval(generateQRCode,1000);
 }
 
- const downloadImg = function(el) {
+
+
+
+
+const downloadImg = function (el) {
   var image = canvas.toDataURL("image/jpg");
   el.href = image;
 };
